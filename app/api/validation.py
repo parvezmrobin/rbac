b098f6a8de1a97c23c23d3cd77ac0be9db5382f8
parvezmrobin@gmail.com <qwer56uiop>
2018-12-15 18:30:08 +0600

    validation/required docstring

--- app/api/validation.py
+++ app/api/validation.py
@@ -1,6 +1,18 @@
-def required(fields, data):
+from typing import Union
+
+
+def required(fields: Union[list, dict], data: dict) -> dict:
+    """
+    Performs a 'required' validation
+    :param fields: fields that are required.
+    :param data: containing the field values
+    :return: fields that are not in data
+    """
     errors = {}
     for field in fields:
         if not (field in data and data[field]):
-            errors[field] = f"{field.capitalize()} is required"
+            if type(fields) == list:
+                errors[field] = f"{field} is required"
+            elif type(fields) == dict:
+                errors[field] = f"{fields[field].capitalize()} is required"
     return errors
