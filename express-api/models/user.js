906a6626c44594d1087c231f5ac7e66cdbd21643
parvezmrobin <parvezmrobin@gmail.com>
2019-01-24 15:48:57 +0600

    + rbac.sqlite

--- express-api/models/user.js
+++ express-api/models/user.js
@@ -9,7 +9,7 @@ function errorHandler(err) {
 }
 
 function getConnection() {
-    const path = join(__dirname, '..', '..', 'rbac.sqlite');
+    const path = join(__dirname, '..', 'rbac.sqlite');
     return new sqlite3.Database(path, errorHandler);
 }
 
@@ -37,26 +37,63 @@ function fetch(query, params, all = true) {
     });
 }
 
-const UserModel = {
-    index: function () {
-        const query = "SELECT id, username, email, first_name, last_name, info FROM user";
-        const params = [];
-        return fetch(query, params).then(rows => {
-            for (let row of rows) {
-                row.info = JSON.parse(row.info);
+function execute(query, params) {
+    return new Promise(function (resolve, reject) {
+        const connection = getConnection();
+        const callback = function cb(err) {
+            connection.close();
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this);
             }
+        };
+        connection.run(query, params, callback);
+    });
+}
 
-            return rows;
-        });
-    },
-    find: function (id) {
-        const query = "SELECT id, username, email, first_name, last_name, info FROM user WHERE id=?";
-        const params = [id];
-        return fetchOne(query, params).then(row => {
-            row.info = JSON.parse(row.info);
-            return row;
-        });
+class UserModel {
+
+
+    static index(withPassword=false) {
+        const query = `SELECT id,username,${withPassword? 'password,': ''}email,first_name,last_name,info FROM user`;
+        const params = [];
+        return fetch(query, params);
     }
-};
+
+    static find(id, withPassword=false) {
+        const query = `SELECT id,username,${withPassword? 'password,': ''}email,first_name,last_name,info` +
+            ' FROM user WHERE id=?';
+        const params = [id];
+        return fetchOne(query, params);
+    }
+
+    static update(user) {
+        const keys = Object.keys(user).map(key => key + '=?').join(',');
+        const query = `UPDATE user SET ${keys} WHERE id=?`;
+        const params = Object.values(user);
+        params.push(user.id);
+        console.log(query, params);
+        return execute(query, params)
+    }
+
+    static where(parameters) {
+        let query = "SELECT id, username, email, first_name, last_name, info FROM user";
+        const keys = Object.keys(parameters);
+        if (keys.length) {
+            query += (" WHERE " + keys[0] + "=?");
+        }
+        for (let i = 1; i < keys.length; i++) {
+            query += " & " + keys[i] + "=?";
+        }
+        const params = Object.values(parameters);
+
+        return fetch(query, params);
+    }
+
+    static whereUsername(username) {
+        return UserModel.where({username: username})
+    }
+}
 
 module.exports = UserModel;
